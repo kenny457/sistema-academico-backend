89e6547346bbf23a4d33d4aa45ba2e5317e766b9
kenny457 <leodanbarrezueta9@gmail.com>
2026-01-10 20:50:01 -0500

    Update database connection settings in db.js

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL || "postgresql://postgres:123456789@localhost:5432/postgres",
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
+
